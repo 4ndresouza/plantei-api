91b2b20696f960b011c5d240af025394849b03df
ADM <ADM@SUL-019834.digitalcollege.internal>
2025-12-02 22:04:36 -0300

    getapp

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 // IMPORTAR MODELS AQUI
 import './models/Produto.js';
 import { sequelize } from './config/database.js';
+import { Produto } from './models/Produto.js';
 
 const HOST = '127.0.0.1'
 const PORT = '5000'
@@ -14,6 +15,56 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API de Produtos funcionando 🚀');
 });
+
+//buscar produtos
+app.get('/produtos', async (req, res) => {
+   try {
+    const produtos =  await Produto.findAll();
+res.status(200).json({mensagem: "Suceso ao trazer os produtos!",
+   
+   size: produtos.length,
+    data: produtos,
+
+});
+   } catch (error) {
+    console.error(error);
+//erros de validação do Sequelize
+if(err.name === 'SequelizeValidationError') {
+    return res.status(400).json({
+        errors: err.errors.map((e) => e.message)
+    });
+
+   }
+   res.status(500).json({ error: 'Erro ao buscar produtos'});
+    
+}
+
+});
+
+//Criar um produto
+app.post('/produto', async (req, res) => {
+
+ try {
+    const payload = req.body;
+    const produto = await Produto.create(payload);
+
+    res.status(500).json({ mensagem: 'Produto criado com sucesso', data: produto });
+
+
+ } catch (error) {
+    console.error(error);
+
+    //erros de validação do Sequelize
+    if (error.name === 'SequelizeValidationError') {
+return res.status(400).json({
+        errors: error.errors.map((e) => e.message)
+});
+ 
+    }
+    res.status(500).json({ error: 'Erro ao criar produto'})
+}
+})
+
 
 try {
     await sequelize.authenticate();
